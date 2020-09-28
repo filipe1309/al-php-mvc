fa3f16d6bace3c0ac085d7d7c3a0c96570908328
Filipe Leuch Bonfim <filipe1309@gmail.com>
2020-09-28 10:55:00 -0300

    Add routes

--- public/index.php
+++ public/index.php
@@ -4,19 +4,14 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use Alura\Cursos\Controller\{ ListarCursos, FormularioInsercao, Persistencia };
 
-switch ($_SERVER['PATH_INFO']) {
-    case '/listar-cursos':
-        $controlador = new ListarCursos();
-        $controlador->processaRequisicao();
-        break;
-    case '/novo-curso':
-        $controlador = new FormularioInsercao();
-        $controlador->processaRequisicao();
-        break;
-    case '/salvar-curso':
-        $controlador = new Persistencia();
-        $controlador->processaRequisicao();
-            break;
-    default:
-        echo "Erro 404";
+$caminho = $_SERVER['PATH_INFO'];
+$rotas =  require __DIR__ . '/../config/routes.php';
+
+if (!array_key_exists($caminho, $rotas)) {
+    http_response_code(404);
+    exit();
 }
+
+$classeControladora = $rotas[$caminho];
+$controlador = new $classeControladora();
+$controlador->processaRequisicao();
