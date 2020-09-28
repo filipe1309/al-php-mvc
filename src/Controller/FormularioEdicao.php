9a2d0f667f81b5c4faa120ac917eb7762b89d063
Filipe Leuch Bonfim <filipe1309@gmail.com>
2020-09-28 18:59:10 -0300

    Create a rende html method

--- src/Controller/FormularioEdicao.php
+++ src/Controller/FormularioEdicao.php
@@ -5,7 +5,7 @@ namespace Alura\Cursos\Controller;
 use Alura\Cursos\Entity\Curso;
 use Alura\Cursos\Infra\EntityManagerCreator;
 
-class FormularioEdicao implements InterfaceControladorRequisicao
+class FormularioEdicao extends ControllerComHtml implements InterfaceControladorRequisicao
 {
     private $repositorioDeCursos;
 
@@ -26,7 +26,9 @@ class FormularioEdicao implements InterfaceControladorRequisicao
 
         $curso = $this->repositorioDeCursos->find($id);
 
-        $titulo = 'Alterar curso ' . $curso->getDescricao();
-        require __DIR__ . '/../../view/cursos/formulario.php';
+        echo $this->renderizaHtml('cursos/formulario.php', [
+            'curso' => $curso,
+            'titulo' => 'Alterar curso ' . $curso->getDescricao(),
+        ]);
     }
 }
