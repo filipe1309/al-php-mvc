a6f86c1fcc96bd1c9db675be5a1169ae00a0457e
Filipe Leuch Bonfim <filipe1309@gmail.com>
2020-09-28 18:41:37 -0300

    Persist update

--- src/Controller/Persistencia.php
+++ src/Controller/Persistencia.php
@@ -23,9 +23,20 @@ class Persistencia implements InterfaceControladorRequisicao
         $curso = new Curso();
         $curso->setDescricao($descricao);
 
-        // inserir no banco
-        $this->entityManager->persist($curso);
+
+        $id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+
+        if (!is_null($id) && $id !== false) {
+            // Atualizar
+            $curso->setId($id);
+            $this->entityManager->merge($curso);
+        } else {
+            // criar
+            // inserir no banco
+            $this->entityManager->persist($curso);
+        }
         $this->entityManager->flush();
+
 
         header('Location: /listar-cursos', true, 302);
     }
